cc8246cc38f0283770e9293245fa571f232043ea
a <a>
2024-02-20 15:03:01 +0530

    added app icon ,change the app name and add the splash screen

--- android/app/src/main/java/com/reactnativeappstructure/MainActivity.kt
+++ android/app/src/main/java/com/reactnativeappstructure/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.reactnativeappstructure
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 class MainActivity : ReactActivity() {
+
+
+override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this) // If you have a custom theme, replace R.style.SplashScreenTheme with your custom theme
+        super.onCreate(savedInstanceState)
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
-import {Modal, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {
+  Modal,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import CustomText from './common/components/customText';
 import CustomButton from './common/components/customButtom';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
   return (
     <View>
       <CustomText text="sameer" />
